e24aaab8aab2a2088ef8edf91fa11dbf92066fc6
庄清惠 <zhuangqhc@163.com>
2017-01-14 23:04:40 +0800

    some template function for smart pointer

---
+++ src/util.h
@@ -0,0 +1,21 @@
+#include <functional>
+#include <memory>
+#include "AST.h"
+
+namespace md {
+
+  using CharStream = std::function<char()>;
+
+  template<typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+
+  template<typename Derived, typename Base>
+  std::unique_ptr<Derived>
+  static_unique_ptr_cast( std::unique_ptr<Base>&& p ) {
+    auto d = static_cast<Derived *>(p.release());
+    return std::unique_ptr<Derived>(d, std::move(p.get_deleter()));
+  }
+
+} // namespace std
